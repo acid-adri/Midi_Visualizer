f1fca732a2461c88e2a0a6c31d5f66fdff9ecfe9
acid-adri <marcmomueller1793@gmail.com>
2025-07-30 13:26:45 +0200

    changed function name to be more precises

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 #include <RtMidi.h>
 #include <ostream>
 
-void getPorts(RtMidiIn *midiin);
+void displayPorts(RtMidiIn *midiin);
 
 int main() {
 	printf("starting!\n");
 	RtMidiIn *midiin = new RtMidiIn();
-	printf("Choose MidiPort");
-	getPorts(midiin);
-
+	printf("Available Ports: ");
+	displayPorts(midiin);
+	
+	printf("Choose Port: ");
 	int j;
 	std::cin >> j;
 	midiin->openPort(j);
@@ -29,7 +30,7 @@ int main() {
 }
 
 //function to display all available midi ports
-void getPorts(RtMidiIn *midiin){
+void displayPorts(RtMidiIn *midiin){
 	int i = 0, midiports = midiin->getPortCount();
 
 	for(i = 0; i < midiports; i++){
